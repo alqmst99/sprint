a1e45446a912746480afa7b9a779dbae4caff644
Nahuel Nicolas Pierini <nahuel.pierini90@hotmail.com>
2023-03-17 19:37:45 -0300

    sprint 1.05.08

--- elements/script/template0.js
+++ elements/script/template0.js
@@ -2,8 +2,6 @@
 
 let templates0=(image,name,description, price, _id, category)=>{
     return ` 
-    
-    
       <div class="card m2 card-box shadow-sm" id="${name}" style="background: linear-gradient(90deg, rgba(226,62,217) 0%, rgba(9,20,168,0.3) 50%, rgba(226,62,217) 100%); border-radius: 10px;"> 
         <figure class="d-block ">
           <img class="card-img-top img-fit rounded" src='${image}' alt="'${name}'">
@@ -16,7 +14,7 @@ let templates0=(image,name,description, price, _id, category)=>{
           <div class="d-flex justify-content-between align-items-center">
             <div class="btn-group">
               <button type="button" class="btn btn-lg btn-outline-primary">
-              <a href="./details.html?id=${id}&&name=${name}&&category=${category}" target="p">ver</a></button>
+              <a href="./details.html?id=${_id}&&name=${name}&&category=${category}" target="p">ver</a></button>
               
             </div>
 
